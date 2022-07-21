a7ce8fe776a0f06441aefad25bd162fad8cd8ba6
Zidong Lu <zilu@microsoft.com>
2022-07-21 16:14:47 +0800

    2

--- test/bdev/bdevperf/bdevperf.c
+++ test/bdev/bdevperf/bdevperf.c
@@ -237,11 +237,11 @@ check_cutoff(void *ctx, uint64_t start, uint64_t end, uint64_t count,
 		return;
 	}
 
-	// so_far_pct = (double)so_far / total;
-	// while (so_far_pct >= **cutoff && **cutoff > 0) {
-	// 	printf("%9.5f%% : %9.3fus\n", **cutoff * 100, (double)end * 1000 * 1000 / g_tsc_rate);
-	// 	(*cutoff)++;
-	// }
+	so_far_pct = (double)so_far / total;
+	while (so_far_pct >= **cutoff && **cutoff > 0) {
+		printf("%9.5f%% : %9.3fus\n", **cutoff * 100, (double)end * 1000 * 1000 / spdk_get_ticks_hz(););
+		(*cutoff)++;
+	}
 }
 
 static void
@@ -544,7 +544,7 @@ bdevperf_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
 	uint64_t		tsc_diff;
 
 	job = task->job;
-	
+
 	tsc_diff = spdk_get_ticks() - task->submit_tsc;
 	if (spdk_unlikely(job->min_tsc > tsc_diff)) {
 		job->min_tsc = tsc_diff;
