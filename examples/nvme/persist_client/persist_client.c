38b3585c6532f2a5cc161fb039871746ae71764e
Zidong Lu <zilu@microsoft.com>
2022-07-29 13:41:54 +0800

    3

--- examples/nvme/persist_client/persist_client.c
+++ examples/nvme/persist_client/persist_client.c
@@ -251,7 +251,7 @@ int main(int argc, char **argv)
 	rc = ibv_query_qp(cm_id->qp, &qp_attr,
 			query_mask, &init_attr);
 	printf("queried psn: %ld\n", qp_attr.sq_psn);
-	qp_attr.sq_psn = lrand48() & 0xffffff;
+	qp_attr.sq_psn = atoi(id);
 
 	rc = ibv_modify_qp(cm_id->qp, &qp_attr, IBV_QP_SQ_PSN);
 	rc = ibv_query_qp(cm_id->qp, &qp_attr,
