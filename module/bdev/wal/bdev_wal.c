e442df34801ed6133d35cfc2669e929652262366
Zidong Lu <zilu@microsoft.com>
2022-08-05 21:21:18 +0800

    fix

--- module/bdev/wal/bdev_wal.c
+++ module/bdev/wal/bdev_wal.c
@@ -1789,7 +1789,7 @@ wal_bdev_mover_update_head(struct spdk_bdev_io *bdev_io, bool success, void *ctx
 			&& bdev->mover_context[i].metadata
 			&& (bdev->mover_context[i].metadata->round < mover_ctx->metadata->round
 				|| (bdev->mover_context[i].metadata->next_offset < mover_ctx->metadata->next_offset 
-					&& bdev->mover_context[i].metadata->round >= mover_ctx->metadata->round))) {
+					&& bdev->mover_context[i].metadata->round == mover_ctx->metadata->round))) {
 				SPDK_DEBUGLOG(bdev_wal, "waiting previous work to update head\n");
 				return;
 			}
