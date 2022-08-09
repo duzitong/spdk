111ba91576f7e4dc15d3f92b48e074b8b41df4d8
Zidong Lu <zilu@microsoft.com>
2022-08-09 09:56:01 +0800

    3

--- module/bdev/wal/bdev_wal.c
+++ module/bdev/wal/bdev_wal.c
@@ -1837,8 +1837,13 @@ wal_bdev_mover_update_head(struct spdk_bdev_io *bdev_io, bool success, void *ctx
 		}
 	}
 
-	assert(bdev->sorted_context[0] == mover_ctx);
 	for (i = 0; i < MAX_OUTSTANDING_MOVES; i++) {
+		if (bdev->sorted_context[i]->state == MOVER_UPDATING_HEAD) {
+			break;
+		}
+	}
+	assert(bdev->sorted_context[i] == mover_ctx);
+	for (; i < MAX_OUTSTANDING_MOVES; i++) {
 		if (bdev->sorted_context[i]->state == MOVER_UPDATING_HEAD) {
 			info->head = bdev->sorted_context[i]->metadata->next_offset;
 			info->round = bdev->sorted_context[i]->metadata->round;
