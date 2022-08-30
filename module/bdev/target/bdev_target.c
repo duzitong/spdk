66fe90e3710ea0c58b8d36bb6753b4365fb1ff27
root <hxy1302@126.com>
2022-08-30 14:39:14 +0800

    1

--- module/bdev/target/bdev_target.c
+++ module/bdev/target/bdev_target.c
@@ -487,9 +487,7 @@ target_rdma_poller(void *ctx)
 						}
 						else {
 							struct spdk_thread* t1 = spdk_bdev_io_get_thread(io), *t2 = spdk_get_thread();
-							SPDK_DEBUGLOG("IO thread %p, current thread %p\n",
-								t1,
-								t2);
+							SPDK_DEBUGLOG("IO thread %p, current thread %p\n", t1, t2);
 							spdk_thread_send_msg(spdk_bdev_io_get_thread(io), target_complete_io_success, io);
 						}
 					}
