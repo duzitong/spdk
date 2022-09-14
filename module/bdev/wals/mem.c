3dddc9c253b6578ff98c986cc0b1aeea2034e6b8
Zidong Lu <zilu@microsoft.com>
2022-09-14 15:57:37 +0800

    debug

--- module/bdev/wals/mem.c
+++ module/bdev/wals/mem.c
@@ -77,14 +77,11 @@ mem_submit_log_write_request(struct wals_target* target, void *data, uint64_t of
     struct wals_mem_target *mem_target = target->private_info;
     memcpy(mem_target->log_buf + offset * mem_target->blocklen, data, cnt * mem_target->blocklen);
 
-    if (cnt <= 2) {
-        SPDK_NOTICELOG("log data: %s$\n", data);
-    }
-
     struct wals_metadata *metadata = data;
     SPDK_NOTICELOG("core write: %ld+%ld\n", metadata->core_offset, metadata->length);
     memcpy(mem_target->core_buf + metadata->core_offset * mem_target->blocklen, data + METADATA_BLOCKS * mem_target->blocklen, metadata->length * mem_target->blocklen);
     if (cnt <= 2) {
+        SPDK_NOTICELOG("src ptr: %p$\n", data + METADATA_BLOCKS * mem_target->blocklen);
         SPDK_NOTICELOG("core data: %s$\n", mem_target->core_buf + metadata->core_offset * mem_target->blocklen);
     }
 
