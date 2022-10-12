ae2bdd8018520995f4c8454dda691155946a4e26
Zidong Lu <zilu@microsoft.com>
2022-10-12 13:33:43 +0800

    fix

--- module/bdev/wals/dma_heap.c
+++ module/bdev/wals/dma_heap.c
@@ -74,34 +74,44 @@ size_t dma_heap_get_buf_size(struct dma_heap *heap)
     return heap->buf_size;
 }
 
-static struct dma_page* _dma_heap_get_page(struct dma_page_ring *ring)
-{
-    struct dma_page *page;
-    if (TAILQ_EMPTY(ring)) {
-        return NULL;
-    }
-
-    page = TAILQ_FIRST(ring);
-    TAILQ_REMOVE(ring, page, link);
-    return page;
-}
-
 struct dma_page* dma_heap_get_page(struct dma_heap *heap, size_t size)
 {
+    struct dma_page *page;
+
     if (size <= SIZE_512) {
-        return _dma_heap_get_page(heap->buf_512);
+        if (TAILQ_EMPTY(&heap->buf_512)) {
+            return NULL;
+        }
+        page = TAILQ_FIRST(&heap->buf_512);
+        TAILQ_REMOVE(&heap->buf_512, page, link);
+        return page;
     }
 
     if (size <= SIZE_4K) {
-        return _dma_heap_get_page(heap->buf_4k);
+        if (TAILQ_EMPTY(&heap->buf_4k)) {
+            return NULL;
+        }
+        page = TAILQ_FIRST(&heap->buf_4k);
+        TAILQ_REMOVE(&heap->buf_4k, page, link);
+        return page;
     }
 
     if (size <= SIZE_8K) {
-        return _dma_heap_get_page(heap->buf_8k);
+        if (TAILQ_EMPTY(&heap->buf_8k)) {
+            return NULL;
+        }
+        page = TAILQ_FIRST(&heap->buf_8k);
+        TAILQ_REMOVE(&heap->buf_8k, page, link);
+        return page;
     }
 
     if (size <= SIZE_64K) {
-        return _dma_heap_get_page(heap->buf_64k);
+        if (TAILQ_EMPTY(&heap->buf_64k)) {
+            return NULL;
+        }
+        page = TAILQ_FIRST(&heap->buf_64k);
+        TAILQ_REMOVE(&heap->buf_64k, page, link);
+        return page;
     }
 
     return NULL;
