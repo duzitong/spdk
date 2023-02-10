5342ff63c84acfe774ade64e6ac0f9ac3b28dc1c
root <hxy1302@126.com>
2023-02-10 14:09:08 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,3 +5,10 @@ git submodule update --init
 ./configure --with-rdma --with-virtio
 apt install -y nvme-cli
 pip3 install --upgrade ansible
+
+./scripts/setup.sh status > tmp.txt
+bdf=$(cat tmp.txt | grep nvme0n1 | awk '{ print $2 }')
+HUGEMEM=32768 PCI_ALLOWED="$bdf" ./scripts/setup.sh
+rm -f tmp.txt
+
+make
