3d727e68d752d1dbe9c672882c6d9c2355facf49
root <hxy1302@126.com>
2023-02-27 10:59:11 +0800

    fix script

--- scripts/parse_bdevperf.py
+++ scripts/parse_bdevperf.py
@@ -5,9 +5,15 @@ import re
 
 IOPS_PREFIX = "         Wals1               :"
 AVERAGE_LATENCY_PREFIX = "Average:"
+MEDIUM_LATENCY_PREFIX = " 50.00000% :"
 
 iops = []
 average_latency = []
+medium_latency = []
+
+iops_cnt = 0
+average_lat_cnt = 0
+medium_lat_cnt = 0
 
 
 if __name__ == "__main__":
@@ -17,18 +23,38 @@ if __name__ == "__main__":
     with open(args.filename, "r") as f:
         for line in f.readlines():
             if line.startswith(IOPS_PREFIX):
-                x = float(
+                iops_cnt += 1
+                if iops_cnt % 2 == 0:
+                    continue
+
+                iops.append(
+                    float(
                         re.search(r'([0-9]*[.])?[0-9]+', line[len(IOPS_PREFIX):]).group()
                     )
-                # small number is from wrong thread
-                if x > 10:
-                    iops.append(x)
+                )
+
             if line.startswith(AVERAGE_LATENCY_PREFIX):
+                average_lat_cnt += 1
+                if average_lat_cnt % 2 == 0:
+                    continue
+
                 average_latency.append(
                     float(
                         re.search(r'([0-9]*[.])?[0-9]+', line[len(AVERAGE_LATENCY_PREFIX):]).group()
                     )
                 )
+            
+            if line.startswith(MEDIUM_LATENCY_PREFIX):
+                medium_lat_cnt += 1
+                if medium_lat_cnt % 2 == 0:
+                    continue
+
+                medium_latency.append(
+                    float(
+                        re.search(r'([0-9]*[.])?[0-9]+', line[len(MEDIUM_LATENCY_PREFIX):]).group()
+                    )
+                )
+
 
     print("IOPS:")
     print('\n'.join([str(x) for x in iops]))
